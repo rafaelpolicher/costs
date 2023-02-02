b80ab418587fdf422b7c913b03d1b0ef0a96c1a8
rafaelpolicher <110497809+RafaelPolicher@users.noreply.github.com>
2023-02-02 17:23:28 -0300

    home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,14 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+
 function Home(){
     return(
-        <h1>Home</h1>
+        <section className={styles.home_container}>
+            <h1>Welcome to <span>Costs</span></h1>
+            <p>Start to manage your projects right now</p>
+            <a href="/">Build a project</a>
+            <img src={savings} alt="Costs" />
+        </section>
     )
 }
 
