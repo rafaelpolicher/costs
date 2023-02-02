f7dc84a951537ed67a160b973c1251bb53799001
rafaelpolicher <110497809+RafaelPolicher@users.noreply.github.com>
2023-02-02 16:52:05 -0300

    navbar and footer

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import Home from './components/pages/Home';
-import Container from './components/pages/layout/Container';
 import NewProject from './components/pages/NewProject';
+
+import Container from './components//layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Projects from './components/pages/Projects';
+
+
 
 
 function App() {
@@ -12,21 +18,17 @@ function App() {
     <Router>  
       <Container customClass="min-height">
       <div className="App">
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/company">Company</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/newproject">NewProject</Link>
-        </div>
+      <Navbar/>
         <Routes>
           <Route path="/" element={<Home />}> </Route>
           <Route path="/company" element={<Company />}> </Route>
+          <Route path="/projects" element={<Projects />}> </Route>
           <Route path="/contact" element={<Contact />}> </Route>
           <Route path="/newProject" element={<NewProject />}> </Route>
         </Routes>
       </div>
       </Container>
-      <p>footer</p>
+      <Footer/>
       </Router>
             
     
