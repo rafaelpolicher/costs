ac41d91e7a7d9c1ed281f345b91a986d00ce4a0e
rafaelpolicher <110497809+RafaelPolicher@users.noreply.github.com>
2023-02-02 18:39:26 -0300

    home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,13 @@
 import styles from './Home.module.css'
 import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
 
 function Home(){
     return(
         <section className={styles.home_container}>
             <h1>Welcome to <span>Costs</span></h1>
             <p>Start to manage your projects right now</p>
-            <a href="/">Build a project</a>
+            <LinkButton to="/newproject" text="Build a project"/>
             <img src={savings} alt="Costs" />
         </section>
     )
