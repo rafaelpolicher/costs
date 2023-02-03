2ed66eab5604c68065b02adb49ea2db87cc97e23
rafaelpolicher <110497809+RafaelPolicher@users.noreply.github.com>
2023-02-03 14:45:27 -0300

    backend

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,8 +1,21 @@
 import ProjectForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
+import { useNavigate } from 'react-router-dom'
 
 
 function NewProject(){
+
+    const history = useNavigate()
+
+    function createPost(project){
+        // initialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch('http://localhost:5000/projects')
+
+    }
+
     return(
         <div className={styles.newproject_container}>
             <h1>Create project</h1>
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,15 +1,34 @@
+import {useEffect, useState} from 'react'
 import Input from '../form/Input.js'
 import Select from '../form/Select.js'
 import SubmitButton from '../form/SubmitButton.js'
 import styles from './ProjectForm.module.css'
+
 function ProjectForm({btnText}){
+
+    const [categories, setCategories] = useState([])
+    
+    useEffect(() => {
+        fetch ('http://localhost:5000/categories', {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+        setCategories(data)
+    })
+    .catch((err) => console.log(err))
+    }, [])
+
     return(
         <form className={styles.form}>
             <Input type='text' text='Project name' name='name' placeholder='Enter the name'/>
 
             <Input type='number' text='Project budget' name='budget' placeholder='Enter the total budget'/>
 
-            <Select name='category_id' text='Select the category'/>
+            <Select name='category_id' text='Select the category' options={categories}/>
 
             <SubmitButton text={btnText} />
         </form>
