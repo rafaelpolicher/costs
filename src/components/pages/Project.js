5616aca21f5e42d512b6c736da5031b69c0cc84c
rafaelpolicher <110497809+RafaelPolicher@users.noreply.github.com>
2023-02-06 15:41:33 -0300

    showproject

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,11 +1,15 @@
 import styles from './Project.module.css'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import Loading from '../layout/Loading'
+import Container from '../layout/Container'
 function Project(){
     const {id} = useParams()
     console.log(id)
 
     const[project, setProject] = useState([])
+
+    const [showProjectForm, setShowProjectForm] = useState(false)
         
     useEffect(()=>{
         fetch(`http://localhost:5000/projects/${id}`,{
@@ -21,8 +25,43 @@ function Project(){
         .catch((err)=> console.log(err))
     },[id])
 
+    function toggleProjectForm(){
+        setShowProjectForm(!showProjectForm)
+    }
     return(
-        <p>{project.name}</p>
+        <>
+            {project.name ? (
+            <div className={styles.project_detials}>
+                <Container customClass='column'>
+                    <div className={styles.details_container}>
+                        <h1>Project: {project.name}</h1>
+                        <button className={styles.btn} onClick={toggleProjectForm}>
+                            {!showProjectForm ? 'Edit project' : 'Close'}
+                        </button>
+                        {!showProjectForm ? (
+                            <div className={styles.project_info}>
+                                <p>
+                                    <span>Category: </span> {project.category.name}
+                                </p>
+                                <p>
+                                    <span>Total Budget: </span> U${project.budget}
+                                </p>
+                                <p>
+                                    <span>Budget used: </span> U${project.cost}
+                                </p>
+                            </div>
+                        ) : (
+                            <div className={styles.project_info}>
+                                <p>Project details</p>
+                            </div>
+                        )}
+                    </div>
+                </Container>
+            </div>
+            ): (
+                <Loading />
+            )}
+        </>
     )
 }
 export default Project
